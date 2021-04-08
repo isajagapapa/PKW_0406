50ad006dfe77ea520089eb645ea6cc293867a120
isss7 <faisfidi@gmail.com>
2021-04-08 13:06:13 +0700

    first commit

---
+++ jsquerybtn.js
@@ -0,0 +1,91 @@
+// ABOUT
+// Menyembunyikan elemen
+$("#hidea").on("click", function() {
+    $("#row").hide();
+});
+
+// Menampilkan elemen
+$("#showa").on("click", function() {
+    $("#row").show();
+});
+
+$("#togglea").on("click", function() {
+    $("#row").toggle();
+});
+
+
+// Fade in
+$("#fadeIna").on("click", function() {
+    $("#row").fadeIn();
+});
+
+// Fade Out
+$("#fadeOuta").on("click", function() {
+    $("#row").fadeOut();
+});
+
+$("#fadeTogglea").on("click", function() {
+    $("#row").fadeToggle();
+});
+
+
+// Slide Up
+$("#slideUpa").on("click", function() {
+    $("#row").slideUp();
+});
+
+// Slide Down
+$("#slideDowna").on("click", function() {
+    $("#row").slideDown();
+});
+
+$("#slideTogglea").on("click", function() {
+    $("#row").slideToggle();
+});
+
+
+
+// PORTOFOLIO
+// Menyembunyikan elemen
+$("#hidegl").on("click", function() {
+    $("#glr").hide();
+});
+
+// Menampilkan elemen
+$("#showgl").on("click", function() {
+    $("#glr").show();
+});
+
+$("#togglegl").on("click", function() {
+    $("#glr").toggle();
+});
+
+
+// Fade in
+$("#fadeIngl").on("click", function() {
+    $("#glr").fadeIn();
+});
+
+// Fade Out
+$("#fadeOutgl").on("click", function() {
+    $("#glr").fadeOut();
+});
+
+$("#fadeTogglegl").on("click", function() {
+    $("#glr").fadeToggle();
+});
+
+
+// Slide Up
+$("#slideUpgl").on("click", function() {
+    $("#glr").slideUp();
+});
+
+// Slide Down
+$("#slideDowngl").on("click", function() {
+    $("#glr").slideDown();
+});
+
+$("#slideTogglegl").on("click", function() {
+    $("#glr").slideToggle();
+});
